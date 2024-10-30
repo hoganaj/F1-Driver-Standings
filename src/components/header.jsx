d7ce13af4fd8218028db4ede3e1b4561ada33d0d
Aidan Hogan <82148170+hoganaj@users.noreply.github.com>
2024-10-30 07:49:25 +0000

    header and background

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import '../App.css';
+import React from 'react';
+
+const Header = () => {
+
+  return (
+    <header className="App-header">
+      <img className="App-logo" src={"/redbullracinglogo.png"} alt="Logo"/>
+    </header>
+  );
+};
+
+export default Header;
